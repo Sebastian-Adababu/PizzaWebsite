6c915bba6138af552ca5b5b2376f255b6435a22c
SEBASTIAN ADABABU <sebastianadababu123@gmail.com>
2024-11-18 15:36:25 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,24 @@
+function filterMenu(category) {
+    const items = document.querySelectorAll('.menu-item');
+    items.forEach(item => {
+        if (category === 'all' || item.classList.contains(category)) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll('a[href^="#"]');
+
+    for (let link of links) {
+        link.addEventListener("click", function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute("href");
+            const targetElement = document.querySelector(targetId);
+            targetElement.scrollIntoView({ behavior: "smooth" });
+        });
+    }
+});
